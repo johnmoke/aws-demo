f2fdaa12442bf35db66ab9a49fe01db5af13afe6
John <makjigoro@gmail.com>
2021-11-24 00:41:54 +0200

    v3 cheanges

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Coming Soon . Website under Construction.
+          Coming Soon . Website under Construction. 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Be on the look!
         </a>
       </header>
     </div>
